df8acf61d664244f966723eeb619f57e78b51df2
stilobique <aurelienvlt@free.fr>
2017-08-24 13:49:08 +0200

    Add a Operator template (code from template blender)

---
+++ Operator_Base.py
@@ -0,0 +1,27 @@
+import bpy
+
+
+class SimpleOperator(bpy.types.Operator):
+    """Insert a simple description"""
+    bl_idname = "object.simple_operator"
+    bl_label = "Simple Object Operator"
+
+    @classmethod
+    def poll(cls, context):
+        return context.active_object is not None
+
+    def execute(self, context):
+        print('Hello World ! You can see me only on the Console')
+        return {'FINISHED'}
+
+
+def register():
+    bpy.utils.register_class(SimpleOperator)
+
+
+def unregister():
+    bpy.utils.unregister_class(SimpleOperator)
+
+
+if __name__ == "__main__":
+    register()
